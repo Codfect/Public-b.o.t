fb7f3f1b0ae885b8df9b55e8e3444b4b6aa04fb7
Oliver <oliverjgodoy@gmail.com>
2020-10-04 17:31:26 -0300

    erro

---
+++ bott/src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Menu.css'
+
+function Menu () {
+    return (
+        <header className="align">
+            <nav>
+                <ul>
+                    <li><a href="/"></a>Home</li>
+                    <li><a href="/"></a>Plans</li>
+                    <li><a href="/"></a>Lorem</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Menu; 
